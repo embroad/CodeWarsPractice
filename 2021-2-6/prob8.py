8271d674e0a63d6996a638755380014a8ea59500
Emily Broad <60267174+embroad@users.noreply.github.com>
2021-05-29 13:26:00 -0700

    Create prob8.py

---
+++ 2021-2-6/prob8.py
@@ -0,0 +1,45 @@
+# 7pts
+# define them all
+# put those boys together
+
+lexiconDictionary = {
+    "enuf":"enough",
+    "ur":"you’re",
+    "u":"you",
+    "cu":"see you",
+    "ub3r":"super",
+    "18r":"later",
+    "gf":"girlfriend",
+    "bf":"boyfriend",
+    "rl":"real life",
+    "d00d":"guy",
+    "m4d":"mad",
+    "r0x0rz":"rocks",
+    "r00lz":"rules",
+    "w00t":"hooray",
+    "pwn":"defeat",
+    "pwnt":"defeated",
+    "t3h":"the",
+    "ph33r":"fear",
+    "gratz":"congratulations",
+    "r1p":"copy",
+    "plz":"please",
+    "mt":"mistell",
+    "ty":"thank you",
+    "re":"welcome back",
+    "0r1y":"oh, really",
+    "eet3d":"ate",
+    "chzbrgr":"cheeseburger",
+    "h4x":"hacks",
+    "1337":"elite",
+    "sk001":"school",
+    "thx":"thanks",
+    "np":"no problem",
+    "meh":"my",
+    "ppl":"people",
+    "l4m3r":"lamer",
+    "sk1llz":"skills"
+}
+
+print(lexiconDictionary["18r"] + ' ' + lexiconDictionary["d00d"] + ' ' + lexiconDictionary["ur"] + ' ' + "so " + lexiconDictionary["ub3r"])
+print(lexiconDictionary["w00t"] + ' ' + lexiconDictionary["meh"] + ' ' + lexiconDictionary["m4d"] + ' ' + lexiconDictionary["1337"] + ' ' + lexiconDictionary["sk1llz"] + ' ' + lexiconDictionary["r00lz"])
