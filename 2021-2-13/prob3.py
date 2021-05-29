dd0b8327d1ab5be50940c009a95e4b53b5a117a7
Emily Broad <60267174+embroad@users.noreply.github.com>
2021-05-29 13:26:58 -0700

    Create prob3.py

---
+++ 2021-2-13/prob3.py
@@ -0,0 +1,31 @@
+# read input moves from sample
+# create board
+# create algorithm for moves
+# make them appear on the board
+# return
+
+import sys
+
+#file = open('2-13-2021\prob3.txt')
+#moves = []
+#moves = file.read()
+
+boardRow1 = "........"
+boardRow2 = "........"
+boardRow3 = "........"
+boardRow4 = "...WB..."
+boardRow5 = "...BW..."
+boardRow6 = "........"
+boardRow7 = "........"
+boardRow8 = "........"
+
+#print(moves)
+
+print(boardRow1)
+print(boardRow2)
+print(boardRow3)
+print(boardRow4)
+print(boardRow5)
+print(boardRow6)
+print(boardRow7)
+print(boardRow8)
